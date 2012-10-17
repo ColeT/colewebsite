d1292bb2d3f813c788d8ca796a1a581c9cb14d39
Cole Terlesky <Cole@agilityfeat.com>
2012-10-17 00:08:49 -0400

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colewebsite::Application.config.secret_token = 'b603a1c08aae00202a14b22d519d3d02e73079d0ef12649e9ae7982d04d28eba1ba8ae6d65a4354d8d136b598cae6c7c3092f5e79fc76b8e0e0a20cc783a0b01'
